db0a3da55cad800d83e125c82e360a890bb6c962
Vladyslav Diachenko <vladislav@nimblecommerce.com>
2018-06-19 23:01:19 +0300

    1 added test

---
+++ src/test/java/com/spd/purchase/PurchaseControllerTest.java
@@ -0,0 +1,41 @@
+package com.spd.purchase;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PurchaseControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void responseShouldContainsOnePurchase() throws Exception {
+		mockMvc.perform(get("/api/purchases"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(APPLICATION_JSON_UTF8))
+				.andExpect(jsonPath("$", hasSize(1)))
+				.andExpect(jsonPath("$[0].id", is(12)))
+				.andExpect(jsonPath("$[0].customerId", is(13)))
+				.andExpect(jsonPath("$[0].productName", is("best product")));
+	}
+}
